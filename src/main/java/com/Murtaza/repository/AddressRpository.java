4619f66bc1812bf7853d0ea6f3f8f4c9b07ce314
XeNoMurtZ <119917285+XeNoMurtZ@users.noreply.github.com>
2023-03-23 19:29:18 +0530

    Delete AddressRpository.java

--- src/main/java/com/Murtaza/repository/AddressRpository.java
+++
@@ -1,9 +0,0 @@
-//package com.vikram.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.vikram.entity.Address;
-//
-//public interface AddressRpository extends JpaRepository<Address, Integer>{
-//
-//}
