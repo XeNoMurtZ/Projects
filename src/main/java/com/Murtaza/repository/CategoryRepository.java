8a11baa5526b00a8619a9f2de96522c75e5c130c
XeNoMurtZ <119917285+XeNoMurtZ@users.noreply.github.com>
2023-03-23 19:29:31 +0530

    Delete CategoryRepository.java

--- src/main/java/com/Murtaza/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vikram.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vikram.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
