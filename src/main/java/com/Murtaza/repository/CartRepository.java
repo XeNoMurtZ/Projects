f992c8aaf04cd90a812408e7c0d833d1a3fef6ae
XeNoMurtZ <119917285+XeNoMurtZ@users.noreply.github.com>
2023-03-23 19:29:27 +0530

    Delete CartRepository.java

--- src/main/java/com/Murtaza/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vikram.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vikram.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer>{
-
-}
